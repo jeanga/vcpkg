b5e455481fa51fd8e9f0c38fcd6427995199d320
Jean Gautier <jean.gautier@ssi.gouv.fr>
2020-08-20 09:50:59 +0200

    7zip & tlsh

--- ports/7zip/portfile.cmake
+++ ports/7zip/portfile.cmake
@@ -12,9 +12,15 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE ${ARCHIVE}
     REF ${7ZIP_VERSION}
     NO_REMOVE_ONE_LEVEL
+    PATCHES
+        add-functions-and-fixes-for-static-link.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/Archive2.def
+    DESTINATION ${SOURCE_PATH}/CPP/7zip/Archive/
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -23,12 +29,22 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/7zip)
+
 vcpkg_copy_pdbs()
 
 file(
     INSTALL ${CMAKE_CURRENT_LIST_DIR}/License.txt
     DESTINATION ${CURRENT_PACKAGES_DIR}/share/7zip
     RENAME copyright
+)
+
+file(
+    COPY
+        ${CMAKE_CURRENT_LIST_DIR}/7zip.h
+        ${CMAKE_CURRENT_LIST_DIR}/guids.h
+        ${CMAKE_CURRENT_LIST_DIR}/extras.h
+    DESTINATION ${CURRENT_PACKAGES_DIR}/include/7zip
 )
 
 vcpkg_test_cmake(PACKAGE_NAME 7zip)
